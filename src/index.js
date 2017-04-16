efad6c098106e997e4efd514d9c8d1abea0354ca
Patrick Riley <priley86@gmail.com>
2017-04-16 17:32:34 -0400

    feat(canvas): Add canvas components and react-dnd
    Add canvas components and react-dnd for drag/drop behavior.
    
    Closes #14

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-export { default as Alert } from './Alert/Alert'
+// Alert
+import Alert from './Alert/Alert'
+
+// Canvas
+import Canvas from './Canvas/Canvas'
+import CanvasItemTypes from './Canvas/CanvasItemTypes'
+import CanvasLayout from './Canvas/CanvasLayout'
+import CanvasNode from './Canvas/CanvasNode'
+import CanvasPanel from './Canvas/CanvasPanel'
+import CanvasToolbar from './Canvas/CanvasToolbar'
+import CanvasTopAlignedToolbox from './Canvas/CanvasTopAlignedToolbox'
+import CustomDragLayer from './Canvas/CustomDragLayer'
+import DraggableCanvasNode from './Canvas/DraggableCanvasNode'
+import DraggableTopAlignedToolboxItem from './Canvas/DraggableTopAlignedToolboxItem'
+
+export {
+  Alert,
+  Canvas,
+  CanvasItemTypes,
+  CanvasLayout,
+  CanvasNode,
+  CanvasPanel,
+  CanvasToolbar,
+  CanvasTopAlignedToolbox,
+  CustomDragLayer,
+  DraggableCanvasNode,
+  DraggableTopAlignedToolboxItem
+}
