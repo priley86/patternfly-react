301fe5322930398180a4080fd9a78dda5c6435e3
Mike Turley <mturley@redhat.com>
2018-03-20 14:21:26 -0400

    Convert comments in helpers.js to JSDoc format

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-// Equivalent to calling `this.someMethod = this.someMethod.bind(this)` for every method name in the methods array.
+/** Equivalent to calling `this.someMethod = this.someMethod.bind(this)` for every method name in the methods array. */
 export const bindMethods = (context, methods) => {
   methods.forEach(method => {
     context[method] = context[method].bind(context);
   });
 };
 
-// Implementation of the debounce function
+/** Implementation of the debounce function */
 export const debounce = (func, wait) => {
   let timeout;
   function innerFunc(...args) {
@@ -18,45 +18,45 @@ export const debounce = (func, wait) => {
   return innerFunc;
 };
 
-// Returns true if propName is a non-null property of the props object (can be any object, not just React props).
+/** Returns true if propName is a non-null property of the props object (can be any object, not just React props). */
 export const propExists = (props, propName) =>
   props && props.hasOwnProperty(propName) && props[propName] !== null;
 
-// Given two objects (props and state), returns the value of propName from props if present, or from state otherwise.
+/** Given two objects (props and state), returns the value of propName from props if present, or from state otherwise. */
 export const propOrState = (props, state, propName) =>
   propExists(props, propName) ? props[propName] : state[propName];
 
-// Returns a subset of the given object including only the given keys, with values optionally replaced by a fn.
+/** Returns a subset of the given object including only the given keys, with values optionally replaced by a fn. */
 export const selectKeys = (obj, keys, fn = val => val) =>
   keys.reduce((values, key) => ({ ...values, [key]: fn(obj[key]) }), {});
 
-// Returns a subset of the given object with a validator function applied to its keys.
+/** Returns a subset of the given object with a validator function applied to its keys. */
 export const filterKeys = (obj, validator) =>
   selectKeys(obj, Object.keys(obj).filter(validator));
 
-// Returns the given React children prop as a regular array of React nodes.
+/** Returns the given React children prop as a regular array of React nodes. */
 export const childrenToArray = children =>
   children &&
   React.Children.count(children) > 0 &&
   React.Children.toArray(children);
 
-// Filters the given React children prop with the given validator function. Returns an array of nodes.
+/** Filters the given React children prop with the given validator function. Returns an array of nodes. */
 export const filterChildren = (children, validator) => {
   const array = childrenToArray(children);
   return array && array.filter(validator);
 };
 
-// Given a React children prop, finds the first child node to pass the validator function.
+/** Given a React children prop, finds the first child node to pass the validator function. */
 export const findChild = (children, validator) => {
   const array = childrenToArray(children);
   return array && array.find(validator);
 };
 
-// Returns true if there is at least one of propNames with a different value in newProps than in oldProps.
+/** Returns true if there is at least one of propNames with a different value in newProps than in oldProps. */
 export const propsChanged = (propNames, oldProps, newProps) =>
   propNames.some(propName => oldProps[propName] !== newProps[propName]);
 
-// Returns an object with the same keys as the given one, but all null values.
+/** Returns an object with the same keys as the given one, but all null values. */
 export const nullValues = obj => selectKeys(obj, Object.keys(obj), () => null);
 
 export const noop = Function.prototype;
