303c5c8b611afd69d66faa1ca02a16238cf514a3
Patrick Riley <priley86@gmail.com>
2019-03-25 18:33:59 -0400

    add autosizer window scroller example

--- packages/patternfly-4/react-virtualized-extension/src/components/Virtualized/examples/AutoSizerExample.js
+++ packages/patternfly-4/react-virtualized-extension/src/components/Virtualized/examples/AutoSizerExample.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import { AutoSizer } from '@patternfly/react-virtualized-extension';
+import { Table, TableHeader } from '@patternfly/react-table';
+import {
+  VirtualizedBody,
+  VirtualizedBodyWrapper,
+  VirtualizedRowWrapper,
+  WindowScroller
+} from '@patternfly/react-virtualized-extension';
 
 class AutoSizerExample extends React.Component {
+  static title = 'Simple Table';
   constructor(props) {
     super(props);
+    const rows = [];
+    for (let i = 0; i < 100; i++) {
+      rows.push({
+        id: i,
+        cells: [`one-${i}`, `two-${i}`, `three-${i}`, `four-${i}`, `five-${i}`]
+      });
+    }
+    this.state = {
+      columns: [
+        { title: 'Repositories' },
+        { title: 'Branches' },
+        { title: 'Pull requests' },
+        { title: 'Workspaces' },
+        { title: 'Last Commit' }
+      ],
+      rows
+    };
   }
 
   render() {
-    return <AutoSizer />;
+    const { columns, rows } = this.state;
+
+    return (
+      <div id="content-scrollable-1" style={{ height: 300, width: '100%', overflow: 'auto' }}>
+        <WindowScroller scrollElement="#content-scrollable-1">
+          {({ height, isScrolling, registerChild, onChildScroll, scrollTop }) => (
+            <Table
+              caption="Simple Table"
+              className="pf-c-virtualized"
+              cells={columns}
+              rows={rows}
+              bodyWrapper={VirtualizedBodyWrapper}
+              rowWrapper={VirtualizedRowWrapper}
+            >
+              <TableHeader />
+              <VirtualizedBody height={height || 400} rowKey="id" />
+            </Table>
+          )}
+        </WindowScroller>
+      </div>
+    );
   }
 }
 
--- packages/patternfly-4/react-virtualized-extension/src/components/Virtualized/index.js
+++ packages/patternfly-4/react-virtualized-extension/src/components/Virtualized/index.js
@@ -2,3 +2,4 @@ export { default as AutoSizer } from './AutoSizer';
 export { default as VirtualizedBody, VirtualizedBodyContext } from './Body';
 export { default as VirtualizedBodyWrapper } from './BodyWrapper';
 export { default as VirtualizedRowWrapper } from './RowWrapper';
+export { default as WindowScroller } from './WindowScroller';
