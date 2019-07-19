1327916aca50c7da5933a1d2c5a74c20d0cb3cfa
jenny-s51 <jshandel12@gmail.com>
2019-07-19 11:02:19 -0400

    fix(integration tests): table test converted to typescript

--- packages/patternfly-4/react-table/src/components/Table/utils/index.d.ts
+++ packages/patternfly-4/react-table/src/components/Table/utils/index.d.ts
@@ -8,7 +8,7 @@ export interface ISortable {
 
 export const sortable: (label: string, extra: IExtra) => ISortable;
 export const headerCol: () => { component: string };
-export const cellWidth: (width: string) => () => { className: string };
+export const cellWidth: (width: string | number) => () => { className: string };
 export const expandable: (value: ReactNode, extra: IExtra) => ReactNode;
 export const isRowExpanded: (row: IRow, rows: Array<IRow>) => boolean | undefined;
 export const compoundExpand: (value: ReactNode, extra: IExtra) => ISortable;
