69bb007b685549b2573da7194a8bc770ca056e14
jenny-s51 <jshandel12@gmail.com>
2019-08-22 15:35:53 -0400

    [WIP] jest tests for VirtualizedTable

--- packages/patternfly-4/react-virtualized-extension/src/components/Virtualized/VirtualTableBody.tsx
+++ packages/patternfly-4/react-virtualized-extension/src/components/Virtualized/VirtualTableBody.tsx
@@ -203,16 +203,15 @@ export default class VirtualTableBody extends React.PureComponent<Props> {
       // note: these aria props if rendered will break a11y for role="presentation"
       // this approach attempts to fix non standard table grids
       // see: https://www.html5accessibility.com/tests/aria-table-fix.html
-      <VirtualGridAny
-        {...this.props}
+      <VirtualGridAny {...this.props}
         style={{
           tableLayout: 'fixed',
           display: 'block',
-          ...style
         }}
         containerStyle={{
           display: 'block'
         }}
+        
         aria-label={null}
         aria-readonly={null}
         tabIndex={null}
@@ -267,8 +266,8 @@ export default class VirtualTableBody extends React.PureComponent<Props> {
   _onScroll = ({ clientHeight, scrollHeight, scrollTop }: VirtualGridScroll) => {
     const { onScroll } = this.props;
 
-    onScroll({ clientHeight, scrollHeight, scrollTop });
-  }
+    onScroll( { clientHeight, scrollHeight, scrollTop } as VirtualGridScroll);
+  };
 
   _onSectionRendered = ({
     rowOverscanStartIndex,
